357a9974f534efd4771e992ce315ed4176f7f9a7
tahmid73 <tahmidislam73@gmail.com>
2022-07-29 15:58:58 +0600

    login

--- src/AllUserComponents/Login.js
+++ src/AllUserComponents/Login.js
@@ -1,7 +1,17 @@
+import axios from "axios";
+import { useState } from "react";
 const Login=()=>{
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const handleLogin=(event)=>{
+        event.preventDefault();
+        const data={email:email,password:password};
+    }
     return(
-        <form>
-            
+        <form onSubmit={handleLogin}>
+            Email: <input onChange={(e)=>{setEmail(e.target.value)}} type="email" name="email" value={email}/> <br/><br/>
+            Password: <input onChange={(e)=>{setPassword(e.target.value)}} type="password" name="password" value={password}/> <br/><br/>
+            <input type="submit" value="Login"/>
         </form>
     )
 }
