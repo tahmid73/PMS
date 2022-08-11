a4ef24f1c98f5538096cd050702993001b1586f3
a-ayeshaa <ayesha.akhtar.1999@gmail.com>
2022-08-11 21:43:22 +0600

    hello

--- src/CustomerComponents/CustomerProfileUpdate.js
+++ src/CustomerComponents/CustomerProfileUpdate.js
@@ -11,7 +11,7 @@ const CustomerProfileUpdate=()=>{
         axiosConfig.post("customer/account").then
         ((rsp)=>{
             setUser(rsp.data);
-            setName(rsp.data.customer_name);
+            // setName(rsp.data.customer_name);
             debugger;
         },(err)=>{
 
@@ -21,7 +21,9 @@ const CustomerProfileUpdate=()=>{
     const updateInfo=(event)=>{
         debugger
         event.preventdefault();
-        const data = {name:name,profilepic:profilepic}
+        const data = {name:name}
+        debugger
+
         axiosConfig.post("/customer/modify/account",data).then
         ((succ)=>{
             debugger;
@@ -34,18 +36,18 @@ const CustomerProfileUpdate=()=>{
             <h3><CustomerTopBar/></h3>
                 <div>
                     <center>
-                    <form onSubmit={updateInfo}>
+                    <form onSubmit={this.updateInfo}>
                         <div>
                         <fieldset style={{width:"50%"}}>
                             <legend style={{textAlign:"center"}}><b>PROFILE INFORMATION</b></legend>
-                            <img width="200" height="250" src={`http://localhost:8000/storage/profilepictures/${user.u_id}.jpg`}/> 
+                            {/* <img width="200" height="250" src={`http://localhost:8000/storage/profilepictures/${user.u_id}.jpg`}/>  */}
                             <br/><br/>
                             <h4>
-                                USER ID : <input type="text" name="u_id" value={user.u_id} readOnly/> <br/> <br/>
+                                {/* USER ID : <input type="text" name="u_id" value={user.u_id} readOnly/> <br/> <br/>
                                 Customer ID : <input type="text" name="c_id" value={user.customer_id} readOnly/> <br/> <br/>
-                                Email : <input type="text" name="email" size={30} value={user.customer_email} readOnly/> <br/><br/>
+                                Email : <input type="text" name="email" size={30} value={user.customer_email} readOnly/> <br/><br/> */}
                                 Name : <input type="text" name="name" onChange={(e)=>{setName(e.target.value)}} value={name} /><br/><br/>
-                                Choose Image : <input type="file" onChange={(e)=>{setProfilePic(e.target.files[0])}} name="profilepic"></input>
+                                {/* Choose Image : <input type="file" onChange={(e)=>{setProfilePic(e.target.files[0])}} name="profilepic"></input> */}
                             </h4> 
                         </fieldset>
                         </div>
